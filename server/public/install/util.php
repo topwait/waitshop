15d937b43153623cd55f15b424983405eba0e9a2
windy <10652944+wafts@user.noreply.gitee.com>
2022-05-06 18:10:31 +0800

    init

--- server/public/install/util.php
+++ server/public/install/util.php
@@ -36,6 +36,6 @@ class Util
      */
     public function loadLock()
     {
-        return file_exists(APP_ROOT . './install.lock');
+        return file_exists(APP_ROOT . '/install.lock');
     }
 }
